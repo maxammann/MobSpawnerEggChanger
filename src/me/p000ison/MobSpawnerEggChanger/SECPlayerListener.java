d9c5c7b3caddd5b79ad1351c256ad8c30ab1785f
p000ison <monstermax.ammann@googlemail.com>
2012-03-15 14:01:54 +0100

    fixed NPEs; Left-click --> Right-click

--- src/me/p000ison/MobSpawnerEggChanger/SECPlayerListener.java
+++ src/me/p000ison/MobSpawnerEggChanger/SECPlayerListener.java
@@ -16,106 +16,107 @@ public class SECPlayerListener implements Listener {
     
     @EventHandler(priority = EventPriority.NORMAL)
     public void onPlayerInteract(PlayerInteractEvent event) {
-    	Action action = event.getAction();
     	Player player = event.getPlayer();
+    	
     	ItemStack iih = event.getItem();
-    	Material blocktype = event.getClickedBlock().getType();
     	Block block = event.getClickedBlock();
     	MobSpawnerEggChanger main = new MobSpawnerEggChanger();
     	
+        if (iih != null && block != null && event.getAction() != null && block.getType() != null) {
     	if (player.isSneaking()) {
 	    	if (player.hasPermission("sec.spawnerchange")) {
-		        if (event.getItem() == null) {
-		            return;
-		        }
-		    	if (blocktype == Material.MOB_SPAWNER && action.equals(Action.LEFT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 50) {
+	    		
+	    		event.setCancelled(true);
+
+		    	if (block.getType() == Material.MOB_SPAWNER && event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 50) {
 		    		((CreatureSpawner)block.getState()).setSpawnedType(EntityType.CREEPER);
 		    		main.sendSpawnerMessage(player, "Creeper");
 		    	}
-		    	if (blocktype == Material.MOB_SPAWNER && action.equals(Action.LEFT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 51) {
+		    	if (block.getType() == Material.MOB_SPAWNER && event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 51) {
 		    		((CreatureSpawner)block.getState()).setSpawnedType(EntityType.SKELETON);
 		    		main.sendSpawnerMessage(player, "Skeleton");
 		    	}
-		    	if (blocktype == Material.MOB_SPAWNER && action.equals(Action.LEFT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 52) {
+		    	if (block.getType() == Material.MOB_SPAWNER && event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 52) {
 		    		((CreatureSpawner)block.getState()).setSpawnedType(EntityType.SPIDER);
 		    		main.sendSpawnerMessage(player, "Spider");
 		    	}
-		    	if (blocktype == Material.MOB_SPAWNER && action.equals(Action.LEFT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 54) {
+		    	if (block.getType() == Material.MOB_SPAWNER && event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 54) {
 		    		((CreatureSpawner)block.getState()).setSpawnedType(EntityType.ZOMBIE);
 		    		main.sendSpawnerMessage(player, "Zombie");
 		    	}
-		    	if (blocktype == Material.MOB_SPAWNER && action.equals(Action.LEFT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 55) {
+		    	if (block.getType() == Material.MOB_SPAWNER && event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 55) {
 		    		((CreatureSpawner)block.getState()).setSpawnedType(EntityType.SLIME);
 		    		main.sendSpawnerMessage(player, "Slime");
 		    	}
-		    	if (blocktype == Material.MOB_SPAWNER && action.equals(Action.LEFT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 56) {
+		    	if (block.getType() == Material.MOB_SPAWNER && event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 56) {
 		    		((CreatureSpawner)block.getState()).setSpawnedType(EntityType.GHAST);
 		    		main.sendSpawnerMessage(player, "Ghast");
 		    	}
-		    	if (blocktype == Material.MOB_SPAWNER && action.equals(Action.LEFT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 57) {
+		    	if (block.getType() == Material.MOB_SPAWNER && event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 57) {
 		    		((CreatureSpawner)block.getState()).setSpawnedType(EntityType.PIG_ZOMBIE);
 		    		main.sendSpawnerMessage(player, "Pig Zombie");
 		    	}
-		    	if (blocktype == Material.MOB_SPAWNER && action.equals(Action.LEFT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 58) {
+		    	if (block.getType() == Material.MOB_SPAWNER && event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 58) {
 		    		((CreatureSpawner)block.getState()).setSpawnedType(EntityType.ENDERMAN);
 		    		main.sendSpawnerMessage(player, "Enderman");
 		    	}
-		    	if (blocktype == Material.MOB_SPAWNER && action.equals(Action.LEFT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 59) {
+		    	if (block.getType() == Material.MOB_SPAWNER && event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 59) {
 		    		((CreatureSpawner)block.getState()).setSpawnedType(EntityType.CAVE_SPIDER);
 		    		main.sendSpawnerMessage(player, "Cave Spider");
 		    	}
-		    	if (blocktype == Material.MOB_SPAWNER && action.equals(Action.LEFT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 60) {
+		    	if (block.getType() == Material.MOB_SPAWNER && event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 60) {
 		    		((CreatureSpawner)block.getState()).setSpawnedType(EntityType.SILVERFISH);
 		    		main.sendSpawnerMessage(player, "Silverfish");
 		    	}
-		    	if (blocktype == Material.MOB_SPAWNER && action.equals(Action.LEFT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 61) {
+		    	if (block.getType() == Material.MOB_SPAWNER && event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 61) {
 		    		((CreatureSpawner)block.getState()).setSpawnedType(EntityType.BLAZE);
 		    		main.sendSpawnerMessage(player, "Blaze");
 		    	}
-		    	if (blocktype == Material.MOB_SPAWNER && action.equals(Action.LEFT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 62) {
+		    	if (block.getType() == Material.MOB_SPAWNER && event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 62) {
 		    		((CreatureSpawner)block.getState()).setSpawnedType(EntityType.MAGMA_CUBE);
 		    		main.sendSpawnerMessage(player, "Magma Cube");
 		    	}
-		    	if (blocktype == Material.MOB_SPAWNER && action.equals(Action.LEFT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 90) {
+		    	if (block.getType() == Material.MOB_SPAWNER && event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 90) {
 		    		((CreatureSpawner)block.getState()).setSpawnedType(EntityType.PIG);
 		    		main.sendSpawnerMessage(player, "Pig");
 		    	}
-		    	if (blocktype == Material.MOB_SPAWNER && action.equals(Action.LEFT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 91) {
+		    	if (block.getType() == Material.MOB_SPAWNER && event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 91) {
 		    		((CreatureSpawner)block.getState()).setSpawnedType(EntityType.SHEEP);
 		    		main.sendSpawnerMessage(player, "Sheep");
 		    	}
-		    	if (blocktype == Material.MOB_SPAWNER && action.equals(Action.LEFT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 92) {
+		    	if (block.getType() == Material.MOB_SPAWNER && event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 92) {
 		    		((CreatureSpawner)block.getState()).setSpawnedType(EntityType.COW);
 		    		main.sendSpawnerMessage(player, "Cow");
 		    	}
-		    	if (blocktype == Material.MOB_SPAWNER && action.equals(Action.LEFT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 93) {
+		    	if (block.getType() == Material.MOB_SPAWNER && event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 93) {
 		    		((CreatureSpawner)block.getState()).setSpawnedType(EntityType.CHICKEN);
 		    		main.sendSpawnerMessage(player, "Chicken");
 		    	}
-		    	if (blocktype == Material.MOB_SPAWNER && action.equals(Action.LEFT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 94) {
+		    	if (block.getType() == Material.MOB_SPAWNER && event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 94) {
 		    		((CreatureSpawner)block.getState()).setSpawnedType(EntityType.SQUID);
 		    		main.sendSpawnerMessage(player, "Squid");
 		    	}
-		    	if (blocktype == Material.MOB_SPAWNER && action.equals(Action.LEFT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 95) {
+		    	if (block.getType() == Material.MOB_SPAWNER && event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 95) {
 		    		((CreatureSpawner)block.getState()).setSpawnedType(EntityType.WOLF);
 		    		main.sendSpawnerMessage(player, "Wolf");
 		    	}
-		    	if (blocktype == Material.MOB_SPAWNER && action.equals(Action.LEFT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 96) {
+		    	if (block.getType() == Material.MOB_SPAWNER && event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 96) {
 		    		((CreatureSpawner)block.getState()).setSpawnedType(EntityType.MUSHROOM_COW);
 		    		main.sendSpawnerMessage(player, "Mushroom Cow");
 		    	}
-		    	if (blocktype == Material.MOB_SPAWNER && action.equals(Action.LEFT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 98) {
+		    	if (block.getType() == Material.MOB_SPAWNER && event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 98) {
 		    		((CreatureSpawner)block.getState()).setSpawnedType(EntityType.OCELOT);
 		    		main.sendSpawnerMessage(player, "Ocelot");
 		    	}
-		    	if (blocktype == Material.MOB_SPAWNER && action.equals(Action.LEFT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 120) {
+		    	if (block.getType() == Material.MOB_SPAWNER && event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG && iih.getDurability() == 120) {
 		    		((CreatureSpawner)block.getState()).setSpawnedType(EntityType.VILLAGER);
 		    		main.sendSpawnerMessage(player, "Villager");
 		    	}
-		    	
-		    	block.getState().update();
-		    	
+		    	if (block.getType() == Material.MOB_SPAWNER && event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+		    		block.getState().update();
+		    	}
 	    	}
     	}
+        }
     }
 }
