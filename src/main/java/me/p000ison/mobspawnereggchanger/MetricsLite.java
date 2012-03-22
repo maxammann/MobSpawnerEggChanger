f26ea3180632ed93292d0380e1532a21076f7e2d
p000ison <monstermax.ammann@googlemail.com>
2012-03-22 16:45:59 +0100

    Removed Unused Imports

--- src/main/java/me/p000ison/mobspawnereggchanger/MetricsLite.java
+++ src/main/java/me/p000ison/mobspawnereggchanger/MetricsLite.java
@@ -1,21 +1,15 @@
 package me.p000ison.mobspawnereggchanger;
 
-import org.bukkit.Bukkit;
-import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.PluginDescriptionFile;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 import java.net.Proxy;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.util.UUID;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginDescriptionFile;
 
 public class MetricsLite {
 
--- src/main/java/me/p000ison/mobspawnereggchanger/MobSpawnerEggChanger.java
+++ src/main/java/me/p000ison/mobspawnereggchanger/MobSpawnerEggChanger.java
@@ -1,14 +1,9 @@
 package me.p000ison.mobspawnereggchanger;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.bukkit.ChatColor;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -30,7 +25,7 @@ public class MobSpawnerEggChanger extends JavaPlugin {
     public void onEnable() {
         PluginManager pm = this.getServer().getPluginManager();
         pm.registerEvents(new SECPlayerListener(), this);
-        
+
         try {
             MetricsLite metrics = new MetricsLite(this);
             metrics.start();
