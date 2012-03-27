9c0d89a1f5d7f3b8f9f88a3ad178e8784361e666
p000ison <monstermax.ammann@googlemail.com>
2012-03-27 18:18:50 +0200

    Update1.4

--- src/main/java/me/p000ison/mobspawnereggchanger/MobSpawnerEggChanger.java
+++ src/main/java/me/p000ison/mobspawnereggchanger/MobSpawnerEggChanger.java
@@ -1,9 +1,11 @@
 package me.p000ison.mobspawnereggchanger;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -16,6 +18,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class MobSpawnerEggChanger extends JavaPlugin {
 
     private static final Logger logger = Logger.getLogger("Minecraft");
+    private File file;
+    private FileConfiguration config;
+    private int SpawnerEggs;
+    private String SpawnerMesssage;
+    private String NotEnoughSpawnerEggs;
+    private String FailedSpawnerEggs;
 
     @Override
     public void onDisable() {
@@ -24,7 +32,11 @@ public class MobSpawnerEggChanger extends JavaPlugin {
     @Override
     public void onEnable() {
         PluginManager pm = this.getServer().getPluginManager();
-        pm.registerEvents(new SECPlayerListener(), this);
+        pm.registerEvents(new SECPlayerListener(this), this);
+
+        file = new File(this.getDataFolder(), "config.yml");
+        config = getConfig();
+        load();
 
         try {
             MetricsLite metrics = new MetricsLite(this);
@@ -34,7 +46,67 @@ public class MobSpawnerEggChanger extends JavaPlugin {
         }
     }
 
+    public void load() {
+        boolean exists = (file).exists();
+
+        if (exists) {
+            try {
+                this.getConfig().options().copyDefaults(true);
+                getConfig().load(file);
+            } catch (Exception ex) {
+                logger.log(Level.WARNING, "Loading the config failed!: " + ex.getMessage());
+            }
+        } else {
+            getConfig().options().copyDefaults(true);
+
+        }
+
+        SpawnerEggs = config.getInt("Global.AmountOfSpawnerEggsYouNeed");
+        SpawnerMesssage = config.getString("Messages.ChangeSpawner");
+        NotEnoughSpawnerEggs = config.getString("Messages.NotEnoughSpawnerEggs");
+        FailedSpawnerEggs = config.getString("Messages.FailedSpawnerEggs");
+
+        save();
+    }
+
+    public void save() {
+        try {
+            getConfig().save(file);
+        } catch (IOException ex) {
+            logger.log(Level.WARNING, "Saving the config failed!: " + ex.getMessage());
+        }
+    }
+
     public void sendSpawnerMessage(Player player, String entity) {
-        player.sendMessage(ChatColor.RED + "[MSEC]" + ChatColor.GRAY + "Spawnertype changed to " + entity.toUpperCase() + ".");
+        player.sendMessage(ChatColor.RED + "[MSEC]" + ChatColor.GRAY + entity);
+    }
+
+    /**
+     * @return the SpawnerEggs
+     */
+    public int getSpawnerEggs() {
+        return SpawnerEggs;
+    }
+
+    /**
+     * @return the SpawnerMesssage
+     */
+    public String getSpawnerMesssage() {
+        return SpawnerMesssage;
+    }
+
+    /**
+     * @return the NotEnoughSpawnerEggs
+     */
+    public String getNotEnoughSpawnerEggs() {
+        return NotEnoughSpawnerEggs;
+    }
+
+    /**
+     * @return the FailedSpawnerEggs
+     */
+    public String getFailedSpawnerEggs() {
+        return FailedSpawnerEggs;
     }
 }
+
--- src/main/java/me/p000ison/mobspawnereggchanger/SECPlayerListener.java
+++ src/main/java/me/p000ison/mobspawnereggchanger/SECPlayerListener.java
@@ -10,10 +10,16 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 public class SECPlayerListener implements Listener {
 
+    private MobSpawnerEggChanger plugin;
+
+    public SECPlayerListener(MobSpawnerEggChanger plugin) {
+        this.plugin = plugin;
+    }
     MobSpawnerEggChanger main = new MobSpawnerEggChanger();
 
     @EventHandler(priority = EventPriority.NORMAL)
@@ -22,23 +28,103 @@ public class SECPlayerListener implements Listener {
         ItemStack iih = event.getItem();
         Block block = event.getClickedBlock();
 
-
-        if (iih != null && block != null && event.getAction() != null && block.getType() != null) {
+        if (iih != null && block != null && event.getAction() != null) {
             if (player.isSneaking()) {
                 if (player.hasPermission("sec.spawnerchange")) {
-                    if (block.getType() == Material.MOB_SPAWNER && event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && iih.getType() == Material.MONSTER_EGG) {
-                        int dur = iih.getDurability();
-                        changeSpawer(dur, block, player);
+                    if (block.getType() == Material.MOB_SPAWNER && event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+                        try {
+                            changeSpawer(iih.getDurability(), block, player, iih);
+                        } catch (Exception ex) {
+                            player.sendMessage(color(plugin.getFailedSpawnerEggs(), (short) 0));
+                        }
                         event.setCancelled(true);
-                        block.getState().update();
                     }
                 }
             }
         }
     }
 
-    public void changeSpawer(int dur, Block block, Player player) {
-        ((CreatureSpawner) block.getState()).setSpawnedType(EntityType.fromId(dur));
-        main.sendSpawnerMessage(player, EntityType.fromId(dur).toString());
+    public void changeSpawer(short dur, Block block, Player player, ItemStack iih) {
+        if (dur != 0) {
+            if (!player.hasPermission("sec.bypass")) {
+                if (iih.getType() == Material.MONSTER_EGG && iih.getAmount() >= plugin.getSpawnerEggs() && iih.getDurability() == dur) {
+                    ((CreatureSpawner) block.getState()).setSpawnedType(EntityType.fromId(dur));
+                    player.sendMessage(color(plugin.getSpawnerMesssage(), dur).replace("%entity", EntityType.fromId(dur).toString()));
+                    remove(player.getInventory(), new ItemStack(Material.MONSTER_EGG), plugin.getSpawnerEggs(), dur);
+                    update(block, player);
+                } else {
+                    player.sendMessage(color(plugin.getNotEnoughSpawnerEggs(), iih.getDurability()).replace("%entity", EntityType.fromId(dur).toString()));
+                }
+            } else {
+                if (iih.getType() == Material.MONSTER_EGG) {
+                    ((CreatureSpawner) block.getState()).setSpawnedType(EntityType.fromId(dur));
+                    player.sendMessage(color(plugin.getSpawnerMesssage(), dur).replace("%entity", EntityType.fromId(dur).toString()));
+                    update(block, player);
+                }
+            }
+        }
+    }
+
+    public void update(Block block, Player player) {
+        block.getState().update();
+        player.updateInventory();
+    }
+
+    public String color(String text, short dur) {
+        String color = text.replaceAll("&(?=[0-9a-fA-FkK])", "\u00a7");
+        return color;
+    }
+
+    /**
+     * @author Acrobot
+     */
+    public static int remove(Inventory inv, ItemStack item, int amount, short durability) {
+        amount = (amount > 0 ? amount : 1);
+        Material itemMaterial = item.getType();
+
+        int first = inv.first(itemMaterial);
+        if (first == -1) {
+            return amount;
+        }
+
+        for (int slot = first; slot < inv.getSize(); slot++) {
+            if (amount <= 0) {
+                return 0;
+            }
+
+            ItemStack currentItem = inv.getItem(slot);
+            if (currentItem == null || currentItem.getType() == Material.AIR) {
+                continue;
+            }
+
+            if (equals(currentItem, item, durability)) {
+                int currentAmount = currentItem.getAmount();
+                if (amount == currentAmount) {
+                    currentItem = null;
+                    amount = 0;
+                } else if (amount < currentAmount) {
+                    currentItem.setAmount(currentAmount - amount);
+                    amount = 0;
+                } else {
+                    currentItem = null;
+                    amount -= currentAmount;
+                }
+                inv.setItem(slot, currentItem);
+            }
+        }
+        return amount;
+    }
+
+    private static boolean equals(ItemStack i, ItemStack item, short durability) {
+        return i != null
+                && i.getType() == item.getType()
+                && (durability == -1 || i.getDurability() == durability);
+    }
+
+    class LivingEntityExeption extends Exception {
+
+        public LivingEntityExeption(String ex) {
+            super(ex);
+        }
     }
 }
