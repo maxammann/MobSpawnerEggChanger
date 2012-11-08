c58d03d9483c3a3d58a4383e8f0dc66db045253e
p000ison <maximilian.ammann@googlemail.com>
2012-11-08 20:22:17 +0100

    Update to 1.4.2

--- src/main/java/me/p000ison/MobSpawnerEggChanger/MobSpawnerEggChanger.java
+++ src/main/java/me/p000ison/MobSpawnerEggChanger/MobSpawnerEggChanger.java
@@ -1,12 +1,11 @@
 package me.p000ison.MobSpawnerEggChanger;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.IOException;
+import java.util.logging.Level;
 
 /**
  * Simply Changes the Spawnertype
@@ -15,8 +14,6 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class MobSpawnerEggChanger extends JavaPlugin {
 
-    private static final Logger logger = Logger.getLogger("Minecraft");
-    private File file;
     private FileConfiguration config;
     private String SpawnerMesssage;
     private String NotEnoughSpawnerEggs;
@@ -24,84 +21,71 @@ public class MobSpawnerEggChanger extends JavaPlugin {
     private boolean perMobPermissions;
 
     @Override
-    public void onDisable() {
+    public void onDisable()
+    {
     }
 
     @Override
-    public void onEnable() {
+    public void onEnable()
+    {
         PluginManager pm = this.getServer().getPluginManager();
         pm.registerEvents(new SECPlayerListener(this), this);
 
-        file = new File(this.getDataFolder(), "config.yml");
-        config = getConfig();
         load();
 
         try {
             MetricsLite metrics = new MetricsLite(this);
             metrics.start();
         } catch (IOException e) {
-            logger.log(Level.WARNING, "Could not send Plugin-Stats: " + e.getMessage());
+            getLogger().log(Level.WARNING, "Could not send Plugin-Stats: " + e.getMessage());
         }
     }
 
-    public void load() {
-        boolean exists = (file).exists();
-
-        if (exists) {
-            try {
-                this.getConfig().options().copyDefaults(true);
-                getConfig().load(file);
-            } catch (Exception ex) {
-                logger.log(Level.WARNING, "Loading the config failed!: " + ex.getMessage());
-            }
-        } else {
-            getConfig().options().copyDefaults(true);
-
-        }
+    public void load()
+    {
+        FileConfiguration config = getConfig();
+        config.options().copyDefaults(true);
+        saveConfig();
 
         SpawnerMesssage = config.getString("Messages.ChangeSpawner");
         NotEnoughSpawnerEggs = config.getString("Messages.NotEnoughSpawnerEggs");
         FailedSpawnerEggs = config.getString("Messages.FailedSpawnerEggs");
         perMobPermissions = config.getBoolean("Global.perMobPermissions");
 
-        save();
-    }
-
-    public void save() {
-        try {
-            getConfig().save(file);
-        } catch (IOException ex) {
-            logger.log(Level.WARNING, "Saving the config failed!: " + ex.getMessage());
-        }
+        saveConfig();
     }
 
     /**
-     * @param typename 
+     * @param typename
      * @return the SpawnerEggs
      */
-    public int getSpawnerEggs(String typename) {
-    	return config.getInt("Global.AmountOfSpawnerEggsYouNeed." + typename.toLowerCase(), 
-    			config.getInt("Global.DefaultAmountOfSpawnerEggsYouNeed"));
+    public int getSpawnerEggs(String typename)
+    {
+        return config.getInt("Global.AmountOfSpawnerEggsYouNeed." + typename.toLowerCase(),
+                config.getInt("Global.DefaultAmountOfSpawnerEggsYouNeed"));
     }
 
     /**
      * @return the SpawnerMesssage
      */
-    public String getSpawnerMesssage() {
+    public String getSpawnerMesssage()
+    {
         return SpawnerMesssage;
     }
 
     /**
      * @return the NotEnoughSpawnerEggs
      */
-    public String getNotEnoughSpawnerEggs() {
+    public String getNotEnoughSpawnerEggs()
+    {
         return NotEnoughSpawnerEggs;
     }
 
     /**
      * @return the FailedSpawnerEggs
      */
-    public String getFailedSpawnerEggs() {
+    public String getFailedSpawnerEggs()
+    {
         return FailedSpawnerEggs;
     }
 
--- src/main/java/me/p000ison/MobSpawnerEggChanger/SECPlayerListener.java
+++ src/main/java/me/p000ison/MobSpawnerEggChanger/SECPlayerListener.java
@@ -13,8 +13,7 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
-public class SECPlayerListener implements Listener
-{
+public class SECPlayerListener implements Listener {
 
     private MobSpawnerEggChanger plugin;
 
@@ -31,26 +30,26 @@ public class SECPlayerListener implements Listener
         Block block = event.getClickedBlock();
 
         if (iih != null && block != null && event.getAction() != null) {
-        	if (player.isSneaking()) {
+            if (player.isSneaking()) {
                 if (player.hasPermission("sec.spawnerchange")) {
                     if (block.getType() == Material.MOB_SPAWNER && event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
                         try {
-                        	String type = EntityType.fromId(iih.getDurability()).getName().toLowerCase();
-                            
-                        	if (plugin.isPerMobPermissions()) {
+                            String type = EntityType.fromId(iih.getDurability()).getName().toLowerCase();
+
+                            if (plugin.isPerMobPermissions()) {
                                 if (player.hasPermission("sec.mob." + type)) {
                                     changeSpawner(block, player, iih);
                                 } else {
-                                	player.sendMessage("You don't have permission to do that!");
+                                    player.sendMessage("You don't have permission to do that!");
                                 }
                             } else {
                                 changeSpawner(block, player, iih);
                             }
 
-                        } catch (Exception ex) {
+                        } catch (RuntimeException ex) {
                             player.sendMessage(color(plugin.getFailedSpawnerEggs(), (short) 0));
                         }
-                        
+
                         event.setCancelled(true);
                     }
                 }
@@ -59,39 +58,35 @@ public class SECPlayerListener implements Listener
     }
 
     @SuppressWarnings("deprecation")
-	public void changeSpawner(Block block, Player player, ItemStack iih) {
-    	Short dur = iih.getDurability();
-    	EntityType type = EntityType.fromId(dur);
-    	String typename = type.getName().toLowerCase();
-    	boolean bypass = player.hasPermission("sec.bypass."+typename);
-    	
+    public void changeSpawner(Block block, Player player, ItemStack iih)
+    {
+        Short dur = iih.getDurability();
+        EntityType type = EntityType.fromId(dur);
+        String typename = type.getName().toLowerCase();
+        boolean bypass = player.hasPermission("sec.bypass." + typename);
+
         if (dur != 0) {
             if (iih.getType() == Material.MONSTER_EGG) {
-            	if (bypass || iih.getAmount() >= plugin.getSpawnerEggs(typename)) {
+                if (bypass || iih.getAmount() >= plugin.getSpawnerEggs(typename)) {
                     ((CreatureSpawner) block.getState()).setSpawnedType(type);
                     player.sendMessage(color(plugin.getSpawnerMesssage(), dur).replace("%entity", typename));
-                    block.getState().update();
-                    
+
                     if (!bypass) {
-	                    remove(player.getInventory(), new ItemStack(Material.MONSTER_EGG), plugin.getSpawnerEggs(typename), dur);
-	                    player.updateInventory();
+                        remove(player.getInventory(), new ItemStack(Material.MONSTER_EGG), plugin.getSpawnerEggs(typename), dur);
+                        player.updateInventory();
                     }
-            	} else {
+                } else {
                     player.sendMessage(color(plugin.getNotEnoughSpawnerEggs(), iih.getDurability()).replace("%entity", EntityType.fromId(dur).toString()));
-                }  
+                }
             }
         }
     }
 
     public String color(String text, short dur)
     {
-        String color = text.replaceAll("&(?=[0-9a-fA-FkK])", "\u00a7");
-        return color;
+        return text.replaceAll("&(?=[0-9a-fA-FkK])", "\u00a7");
     }
 
-    /**
-     * @author Acrobot
-     */
     public static int remove(Inventory inv, ItemStack item, int amount, short durability)
     {
         amount = (amount > 0 ? amount : 1);
